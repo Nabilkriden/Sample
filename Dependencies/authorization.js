1c3cb2bfddab576b5edd7f900f34049ff1d96260
unknown <aminebenzid2003@gmail.com>
2022-02-16 15:56:28 +0100

    roles

--- Dependencies/authorization.js
+++ Dependencies/authorization.js
@@ -1,65 +1,70 @@
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+//login validation
+exports.verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  const JWT_KEY = process.env.JWT_KEY;
+  if (token) {
+    try {
+      jwt.verify(token, JWT_KEY);
+      next();
+    } catch {
+      return res.status(403).send({ result: "access denied !!!" });
+    }
+  } else res.status(400).send({ result: "access denied !!!" });
+};
+
 //Super Admin
 exports.verifySuperAdminToken = async (req, res, next) => {
-  try {
-    var token = req.headers.authorization;
-    if (!token) res.status(400).send({ result: "access denied !!!" });
+  const token = req.headers.authorization;
+  const JWT_KEY = process.env.JWT_KEY;
+  if (token) {
     const user = jwt.decode(token, { comlete: true });
     const userRole = user.role;
-    const JWT_KEY = process.env.JWT_KEY;
-    if (token && userRole == "super_admin") {
+    if (userRole === "super_admin") {
       try {
         jwt.verify(token, JWT_KEY);
         next();
       } catch (err) {
-        res.send({ error: err.message }).status(500);
+        return res.status(403).send({ result: "access denied !!!" });
       }
     } else return res.status(400).send({ result: "access denied !!!" });
-  } catch {
-    return res.status(403).send({ result: "access denied !!!" });
-  }
+  } else return res.status(400).send({ result: "access denied !!!" });
 };
 
 // Admin
 exports.verifyAdminToken = async (req, res, next) => {
-  try {
-    var token = req.headers.authorization;
-    if (!token) res.status(400).send({ result: "access denied !!!" });
+  const token = req.headers.authorization;
+  const JWT_KEY = process.env.JWT_KEY;
+  if (token) {
     const user = jwt.decode(token, { comlete: true });
     const userRole = user.role;
-    const JWT_KEY = process.env.JWT_KEY;
-    if (token && userRole == "admin") {
+    if (userRole === "admin") {
       try {
         jwt.verify(token, JWT_KEY);
         next();
       } catch (err) {
-        res.send({ error: err.message }).status(500);
+        return res.status(403).send({ result: "access denied !!!" });
       }
     } else return res.status(400).send({ result: "access denied !!!" });
-  } catch {
-    return res.status(403).send({ result: "access denied !!!" });
-  }
+  } else return res.status(400).send({ result: "access denied !!!" });
 };
 
 // assistant
 exports.verifyAssistantToken = async (req, res, next) => {
-  try {
-    var token = req.headers.authorization;
-    if (!token) res.status(400).send({ result: "access denied !!!" });
+  const token = req.headers.authorization;
+  const JWT_KEY = process.env.JWT_KEY;
+  if (token) {
     const user = jwt.decode(token, { comlete: true });
     const userRole = user.role;
-    const JWT_KEY = process.env.JWT_KEY;
-    if (token && userRole == "assistant") {
+    if (userRole === "assistant") {
       try {
         jwt.verify(token, JWT_KEY);
         next();
       } catch (err) {
-        res.send({ error: err.message }).status(500);
+        return res.status(403).send({ result: "access denied !!!" });
       }
     } else return res.status(400).send({ result: "access denied !!!" });
-  } catch {
-    return res.status(403).send({ result: "access denied !!!" });
-  }
+  } else return res.status(400).send({ result: "access denied !!!" });
 };
