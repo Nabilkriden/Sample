382d671bde46c4084126bb2720306de410d550a5
unknown <aminebenzid2003@gmail.com>
2022-02-16 13:47:02 +0100

    login/inv

--- Controllers/loginController.js
+++ Controllers/loginController.js
@@ -17,30 +17,30 @@ exports.login = async (req, res, next) => {
   try {
     const user = await Users.findOne({ email: email });
     if (!user) {
-      res.status(400).send("Email or password is incorrect.");
-      return;
+      return res.status(400).send("Email or password is incorrect.");
     }
-    if (user) {
-      const validPasswords = await bcrypt.compare(password, user.password);
-      if (!validPasswords) {
-        return res.status(400).send("Email or password is incorrect.");
-      }
-      if (validPasswords) {
-        const code = codeGen("Num", 8);
-        mailer(email, "Simple Login ", `This is your code ${code}`);
+    const validPasswords = await bcrypt.compare(password, user.password);
 
-        try {
-          const addCode = await Users.findOneAndUpdate(
-            { email: email },
-            { code: code }
-          );
-          if (addCode) {
-            res.status(200).send({ result: "Code send", code: user.code });
-          }
-        } catch (err) {
-          return res.status(500).send(err);
+    if (user && validPasswords) {
+      const code = codeGen("Num", 8);
+      mailer(email, "Simple Login ", `This is your code ${code}`);
+
+      try {
+        const addCode = await Users.findOneAndUpdate(
+          { email: email },
+          { code: code }
+        );
+        if (addCode) {
+          res.status(200).send({
+            result: "Verification Code Sent Successfully",
+            code: user.code,
+          });
         }
+      } catch (err) {
+        return res.status(500).send(err);
       }
+    } else {
+      return res.status(400).send("Email or password is incorrect.");
     }
   } catch (err) {
     return res.status(500).send(err);
@@ -48,28 +48,25 @@ exports.login = async (req, res, next) => {
   next();
 };
 exports.codeVerif = async (req, res, next) => {
-  // Taking the code from the user
   const email = req.body.email;
   const code = req.body.code;
   try {
     const user = await Users.findOne({ email: email });
-    if (user) {
-      if (user.code === code) {
-        try {
-          const JWT_KEY = process.env.JWT_KEY;
-          const token = jwt.sign(
-            { userId: user._id, userName: user.name },
-            JWT_KEY
-          );
-          res
-            .send({ result: "user loged in", user: user, token: token })
-            .status(200);
-        } catch (err) {
-          res.send({ err }).status(500);
-        }
-      } else {
-        return res.status(401).send({ err: "Faild To Login" });
+    if (user && user.code === code) {
+      try {
+        const JWT_KEY = process.env.JWT_KEY;
+        const token = jwt.sign(
+          { userId: user._id, userName: user.name },
+          JWT_KEY
+        );
+        res
+          .send({ result: "user loged in", user: user, token: token })
+          .status(200);
+      } catch (err) {
+        res.send({ err }).status(500);
       }
+    } else {
+      return res.status(401).send({ err: "Faild To Login" });
     }
   } catch (err) {
     res.status(500).send(err);
